b7519c95dd083608a5580aafd5e9b22ecf767fa2
Unknown <m.kudamatsu@gmail.com>
2018-04-11 14:51:00 +0900

    Extract the border from the original map #2

---
+++ code/karabakh-border.py
@@ -0,0 +1,74 @@
+print "Setting the working directory"
+import os
+work_dir = os.path.dirname(os.path.realpath(__file__)) # This method returns the directry path of this script.
+os.chdir(work_dir)
+
+
+### Define the main function ###
+def main():
+  try:
+    print "Launching ArcGIS"
+    import arcpy
+    print "Enabling the Spatial Analyst extension"
+    from arcpy.sa import *
+    arcpy.CheckOutExtension("Spatial")
+    print "Setting the environment"
+    arcpy.env.overwriteOutput = True # Allow the overwriting of the output files
+    arcpy.env.workspace = "../temp/" # NEVER USE single backslash (\). # Set the working directory.
+
+    print "Defining input and output macro names"
+    # Input
+    rgb_raster = "Map_Artsakh_(NKR)_en_equidistant_conic_395_400_cm440.tiff"
+    # Intermediate
+    red_raster = "red.tif"
+    green_raster = "green.tif"
+    blue_raster = "blue.tif"
+    # Output
+    border_raster = "karabakh-border.tif"
+
+    print "Extracting red values"
+    rgb_to_color(rgb_raster, red_raster, "red")
+    print "Extracting green values"
+    rgb_to_color(rgb_raster, green_raster, "green")
+    print "Extracting blue values"
+    rgb_to_color(rgb_raster, blue_raster, "blue")
+    print "Extracting the border's red value locations"
+    red_value_for_border = "VALUE = 171"
+    red_object = Con(Raster(red_raster), 1, 0, red_value_for_border)
+    print "Extracting the border's green value locations"
+    green_value_for_border = "VALUE = 52"
+    green_object = Con(Raster(green_raster), 1, 0, green_value_for_border)
+    print "Extracting the border's blue value locations"
+    blue_value_for_border = "VALUE = 69"
+    blue_object = Con(Raster(blue_raster), 1, 0, blue_value_for_border)
+    print "Extracting the border"
+    border_raster_object = red_object * green_object * blue_object
+    print "Saving the raster object"
+    border_raster_object.save(border_raster)
+
+    ### Release the memory ###
+    del arcpy
+    print "All done."
+
+  except:
+    print arcpy.GetMessages()
+
+# arcpy.CopyRaster_management(red, karabakh_red, "", "", "256", "NONE", "NONE", "", "NONE", "NONE", "GRID", "NONE")
+def rgb_to_color(in_raster, out_ras, color):
+  print "Setting the color"
+  if color == "red":
+      color_number = "1"
+  if color == "green":
+      color_number = "2"
+  if color == "blue":
+      color_number = "3"
+  print "... making raster layer"
+  # arcpy.MakeRasterLayer_management(in_raster, out_ras, band_index = color_number)
+  arcpy.MakeRasterLayer_management(in_raster, "temp_layer", band_index = color_number)
+  # See http://pro.arcgis.com/en/pro-app/tool-reference/data-management/make-raster-layer.htm for the tool help
+  print "... saving as a tiff file"
+  arcpy.CopyRaster_management("temp_layer", out_ras)
+
+### Execute the main function ###
+if __name__ == "__main__":
+    main()
